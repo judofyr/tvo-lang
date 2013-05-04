15873e512ad162897961b1ab67d0add3c3413ea7
Magnus Holm <judofyr@gmail.com>
2013-05-04 22:36:59 +0200

    Add rec tests

--- test/test_tvo.rb
+++ test/test_tvo.rb
@@ -10,10 +10,25 @@ module Tvo
       assert_equal 1, tvo('1')
       assert_equal 4, tvo('2 2 +')
       assert_equal 2, tvo('4 2 /')
+      assert tvo('2 2 =')
+      refute tvo('2 3 =')
     end
 
     def test_dip
       assert_equal 3, tvo('1 2 4 [+] dip pop')
+    end
+
+    def test_branch
+      assert_equal 1, tvo('2 2 = 1 2 ?')
+      assert_equal 2, tvo('1 2 = 1 2 ?')
+    end
+
+    def test_rec_field
+      assert_equal 'Hello', tvo('rec  "Hello" =text  .text')
+    end
+
+    def test_rec_method
+      assert_equal 'Hello', tvo('rec  :hello "Hello" ;  hello')
     end
   end
 end
